17806236ad1900cda8de1377800688b727c4133c
Paul Batum <paul.batum@gmail.com>
2016-06-13 14:41:29 -0700

    Initial commit of Xamarin.Forms client. XAML designer works and app loads in iOS.

---
+++ SnapAndSave/SnapAndSaveClient/SnapAndSave/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SnapAndSave
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ SnapAndSave/SnapAndSaveClient/SnapAndSave/SnapAndSave.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SnapAndSave
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			var content = new MainPage ();
+
+			MainPage = new NavigationPage (content);
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
